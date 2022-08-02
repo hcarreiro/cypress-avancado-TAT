ed06687213ad0b54b737b61addaa7e1390740b4b
Hildefonso Carreiro <hildefonso.carreiro@gmail.com>
2022-08-02 13:42:06 -0300

    refatoração incluindo context que bate na api real

--- cypress/integration/hackerStories.spec.js
+++ cypress/integration/hackerStories.spec.js
@@ -2,57 +2,48 @@
 
 describe('Hacker Stories', () => {
 
-  // beforeEach 1
-  beforeEach(() => {
+  const initialTerm = 'React'
+  const newTerm = 'Cypress'
 
-    // Aqui estamos setando um ponto de interceptação onde vamos interceptar uma requisição
-    //cy.intercept(
-    // Tipo da requisição que estaremos monitorando
-    //'GET',
-    // Rota que estaremos monitorando
-    //'**/search?query=React&page=0'
-    // Daremos um alias para essa requisição
-    //).as('getStories')
+  // Contexto para armazenar as batidas na API real
+  context('Hitting the real API', () => {
 
-    // Nova implementação utilizando um objeto e query strings
-    cy.intercept({
-      // Método a ser monitorado
-      method: 'GET',
-      // path entre a urlBase e a query string
-      pathname: '**/search',
-      // Objeto query com query e page
-      query: {
-        query: 'React',
-        page: '0'
-      }
-    }).as('getStories')
-
-
-    // Acessamos a raiz do nosso sisitema
-    cy.visit('/')
-    // Damos um wait aguardando o @getStories que é o alias da requisição GET com o endereço declarados acima.
-    cy.wait('@getStories')
-
-    // Simulamos um cenário onde acessamos a aplicação e aguardamos o carregamento da página.
-
-    // CÓDIGO DEFASADO
-    //cy.assertLoadingIsShownAndHidden()
-    //cy.contains('More').should('be.visible')
-  })
-
-  it('shows the footer', () => {
-    cy.get('footer')
-      .should('be.visible')
-      .and('contain', 'Icons made by Freepik from www.flaticon.com')
-  })
-
-  context('List of stories', () => {
-    // Since the API is external,
-    // I can't control what it will provide to the frontend,
-    // and so, how can I assert on the data?
-    // This is why this test is being skipped.
-    // TODO: Find a way to test it out.
-    it.skip('shows the right data for all rendered stories', () => { })
+    // beforeEach 1
+    beforeEach(() => {
+  
+      // Aqui estamos setando um ponto de interceptação onde vamos interceptar uma requisição
+      //cy.intercept(
+      // Tipo da requisição que estaremos monitorando
+      //'GET',
+      // Rota que estaremos monitorando
+      //'**/search?query=React&page=0'
+      // Daremos um alias para essa requisição
+      //).as('getStories')
+  
+      // Nova implementação utilizando um objeto e query strings
+      cy.intercept({
+        // Método a ser monitorado
+        method: 'GET',
+        // path entre a urlBase e a query string
+        pathname: '**/search',
+        // Objeto query com query e page
+        query: {
+          query: initialTerm,
+          page: '0'
+        }
+      }).as('getStories')  
+  
+      // Acessamos a raiz do nosso sisitema
+      cy.visit('/')
+      // Damos um wait aguardando o @getStories que é o alias da requisição GET com o endereço declarados acima.
+      cy.wait('@getStories')
+  
+      // Simulamos um cenário onde acessamos a aplicação e aguardamos o carregamento da página.
+  
+      // CÓDIGO DEFASADO
+      //cy.assertLoadingIsShownAndHidden()
+      //cy.contains('More').should('be.visible')
+    })
 
     it('shows 20 stories, then the next 20 after clicking "More"', () => {
 
@@ -64,7 +55,7 @@ describe('Hacker Stories', () => {
         pathname: '**/search',
         // Objeto query com query e page
         query: {
-          query: 'React',
+          query: initialTerm,
           // Valor 1 pois é o resultado quando clicamos no botão 'more'
           page: '1'
         }
@@ -79,6 +70,53 @@ describe('Hacker Stories', () => {
 
       cy.get('.item').should('have.length', 40)
     })
+
+    it('searches via the last searched term', () => {
+
+      // Aqui estamos setando um ponto de interceptação onde vamos interceptar uma requisição
+      cy.intercept(
+        // Tipo da requisição que estaremos monitorando
+        'GET',
+        // Rota que estaremos monitorando
+        `**/search?query=${newTerm}&page=0`
+        // Daremos um alias para essa requisição
+      ).as('getNewTermStories')
+
+      cy.get('#search')
+        .clear()
+        .type(`${newTerm}{enter}`)
+
+      // Nova implementação
+      cy.wait('@getNewTermStories')
+
+      cy.get(`button:contains(${initialTerm})`)
+        .should('be.visible')
+        .click()
+
+      cy.wait('@getStories')
+
+      cy.get('.item').should('have.length', 20)
+      cy.get('.item')
+        .first()
+        .should('contain', initialTerm)
+      cy.get(`button:contains(${newTerm})`)
+        .should('be.visible')
+    })
+
+    it('shows the footer', () => {
+      cy.get('footer')
+        .should('be.visible')
+        .and('contain', 'Icons made by Freepik from www.flaticon.com')
+    })
+  })
+
+  context('List of stories', () => {
+    // Since the API is external,
+    // I can't control what it will provide to the frontend,
+    // and so, how can I assert on the data?
+    // This is why this test is being skipped.
+    // TODO: Find a way to test it out.
+    it.skip('shows the right data for all rendered stories', () => { })
 
     it('shows only nineteen stories after dimissing the first story', () => {
       cy.get('.button-small')
@@ -126,7 +164,7 @@ describe('Hacker Stories', () => {
           .should('be.visible')
       })
 
-      it('shows "Something went wrong ..." in case of a network error', () => {
+      it.skip('shows "Something went wrong ..." in case of a network error', () => {
 
         // Geramos um cy.intercep para interceptar qualquer requisição GET para este endereço e retornar agora um erro de rede
         cy.intercept(
@@ -144,15 +182,11 @@ describe('Hacker Stories', () => {
         // A aplicação neste caso exibe uma msg de erro a estamos validando se ela está visível
         cy.get(`p:contains(${errorMessage})`)
           .should('be.visible')
-
       })
-
     })
   })
 
   context('Search', () => {
-    const initialTerm = 'React'
-    const newTerm = 'Cypress'
 
     // beroreEach 2
     beforeEach(() => {
@@ -216,27 +250,6 @@ describe('Hacker Stories', () => {
     })
 
     context('Last searches', () => {
-
-      it('searches via the last searched term', () => {
-        cy.get('#search')
-          .type(`${newTerm}{enter}`)
-
-        // Nova implementação
-        cy.wait('@getNewTermStories')
-
-        cy.get(`button:contains(${initialTerm})`)
-          .should('be.visible')
-          .click()
-
-        cy.wait('@getStories')
-
-        cy.get('.item').should('have.length', 20)
-        cy.get('.item')
-          .first()
-          .should('contain', initialTerm)
-        cy.get(`button:contains(${newTerm})`)
-          .should('be.visible')
-      })
 
       it('shows a max of 5 buttons for the last searched terms', () => {
         const faker = require('faker')
